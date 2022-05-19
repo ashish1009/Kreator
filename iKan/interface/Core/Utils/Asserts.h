f4981fa48d3ac2d0659a61b7f0ea3b2bbd7b3c08
Ashish <aashishkan1009@gmail.com>
2022-05-19 14:50:29 +0530

    Adding Assert API for both core and client to trigger Exception

---
+++ iKan/interface/Core/Utils/Asserts.h
@@ -0,0 +1,23 @@
+//
+//  Asserts.h
+//  iKan
+//
+//  Created by iKan on 19/05/22.
+//
+
+#pragma once
+
+#define IK_EXPAND_VARGS(x) x
+
+#define ASSERT(condition) assert(condition);
+
+#define IK_ASSERT_NO_MESSAGE(condition) { if(!(condition)) { IK_ERROR("Assertion Failed"); ASSERT(condition); } }
+#define IK_ASSERT_MESSAGE(condition, ...) { if(!(condition)) { IK_ERROR("Assertion Failed: {0}", __VA_ARGS__); ASSERT(condition); } }
+
+#define IK_ASSERT_RESOLVE(arg1, arg2, macro, ...) macro
+
+#define IK_GET_ASSERT_MACRO(...) IK_EXPAND_VARGS(IK_ASSERT_RESOLVE(__VA_ARGS__, IK_ASSERT_MESSAGE, IK_ASSERT_NO_MESSAGE))
+
+#define IK_ASSERT(...) IK_EXPAND_VARGS( IK_GET_ASSERT_MACRO(__VA_ARGS__)(__VA_ARGS__) )
+
+#define IK_ABORT() IK_ASSERT_NO_MESSAGE(false);
